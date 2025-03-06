f7d944f0a650836dc09893c6e680df23fa182392
brotherofken <brotherofken@gmail.com>
2025-03-06 12:49:46 +0100

    Do requests call in thread pool

--- demos/python/sdk_wireless_camera_control/open_gopro/gopro_base.py
+++ demos/python/sdk_wireless_camera_control/open_gopro/gopro_base.py
@@ -6,6 +6,7 @@
 from __future__ import annotations
 
 import asyncio
+import concurrent.futures
 import enum
 import functools
 import json
@@ -120,6 +121,7 @@ class GoProBase(GoProHttp, Generic[ApiType]):
     def __init__(self, **kwargs: Any) -> None:
         self._should_maintain_state = kwargs.get("maintain_state", True)
         self._exception_cb = kwargs.get("exception_cb", None)
+        self._pool = concurrent.futures.ThreadPoolExecutor(max_workers=20)
 
     async def __aenter__(self: GoPro) -> GoPro:
         await self.open()
@@ -384,7 +386,7 @@ class GoProBase(GoProHttp, Generic[ApiType]):
                 # http_response = requests.get(url, timeout=timeout, **self._build_http_request_args(message))
                 loop = asyncio.get_event_loop()
                 http_response = await loop.run_in_executor(
-                    None, functools.partial(requests.get, url, timeout=timeout, **self._build_http_request_args(message))
+                    self._pool, functools.partial(requests.get, url, timeout=timeout, **self._build_http_request_args(message))
                 )
                 logger.trace(f"received raw json: {json.dumps(http_response.json() if http_response.text else {}, indent=4)}")  # type: ignore
                 if not http_response.ok:
